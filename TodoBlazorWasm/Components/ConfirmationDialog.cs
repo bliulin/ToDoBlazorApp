1e72051d0542740bc7967de2b96ba296230729e5
bliulin <bliulinx@gmail.com>
2023-03-06 22:47:27 +0200

    confirmation dialog

---
+++ TodoBlazorWasm/Components/ConfirmationDialog.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TodoBlazorWasm.Components
+{
+    public partial class ConfirmationDialog
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Content { get; set; }
+
+        protected bool ShowDialog { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CloseEventCallback { get; set; }
+
+        public void Show()
+        {
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+
+        public async void OnOkClicked()
+        {
+            ShowDialog = false;            
+            await this.CloseEventCallback.InvokeAsync(true);
+            StateHasChanged();
+        }
+
+        public void OnCancelClicked()
+        {
+            this.Close();
+        }
+    }
+}
--- TodoBlazorWasm/Pages/Index.cs
+++ TodoBlazorWasm/Pages/Index.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 using Todo.Shared;
+using TodoBlazorWasm.Components;
 
 namespace TodoBlazorWasm.Pages
 {
     public partial class Index
     {
+        private ToDoItem _toDelete;
+        private string _confirmationDeleteText = "";
+
         public List<ToDoItem> Items { get; set; } = new List<ToDoItem>();
 
         [Inject]
         public HttpClient HttpClient { get; set; }
+
+        protected ConfirmationDialog ConfirmationDeleteDialog { get; set; }
+
+        protected string ConfirmationText => _confirmationDeleteText;
 
         protected override async Task OnInitializedAsync()
         {
@@ -19,7 +27,20 @@ namespace TodoBlazorWasm.Pages
 
         protected async Task DeleteTodo(ToDoItem item)
         {
-            await HttpClient.DeleteAsync($"/todos/{item.Id}");
+            _toDelete = item;
+            _confirmationDeleteText = $"Are you sure you want to delete '{item.Title}'?";
+            ConfirmationDeleteDialog.Show();
+        }
+
+        protected async void OnConfirmationDeleteTodoDialogClosed(bool arg)
+        {
+            await RemoveTodo(_toDelete.Id.Value);
+            StateHasChanged();
+        }
+
+        private async Task RemoveTodo(Guid id)
+        {
+            await HttpClient.DeleteAsync($"/todos/{id}");
             var array = await HttpClient.GetFromJsonAsync<ToDoItem[]>("/todos");
             Items = new List<ToDoItem>(array);
             this.StateHasChanged();
